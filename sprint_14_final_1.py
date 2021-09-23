2c76953d3341e303118b9ac03b063795b702d0d0
Ковылин Василий <coherentus@gmail.com>
2021-09-23 18:19:38 +0300

    kjhjkh

--- sprint_14_final_1.py
+++ sprint_14_final_1.py
@@ -2,40 +2,6 @@ def broken_search(nums, target) -> int:
     #  Your code
     #  “ヽ(´▽｀)ノ”
     
-    def bin_search(arr, x, left, right):
-        arr_len = len(arr)
-        if arr_len < 4:
-            for i in range(arr_len):
-                if arr[i][1] == x:
-                    return arr[i][0]
-            return -1
-
-        # center
-        mid = (left + right) // 2
-        # sorted half ?
-        if arr[mid][1] >= arr[left][1]: # sorted            
-            # check x in sorted
-            if arr[left][1] <= x <= arr[mid][1]:
-                if arr[mid][1] == x:
-                    return arr[mid][0]
-                if arr[left][1] == x:
-                    return arr[left][0]
-                return binary_search(arr, x, left + 1, mid)
-            unsorted = arr[mid + 1: right + 1]
-        else:
-            # sorted from mid to right
-            # check x in sorted
-            if arr[mid][1] <= x <= arr[right][1]:
-                if arr[mid][1] == x:
-                    return arr[mid][0]
-                if arr[right][1] == x:
-                    return arr[right][0]
-                return binary_search(arr, x, mid + 1, right)
-            unsorted = arr[left: mid]
-        
-        return bin_search(unsorted, x, 0, len(unsorted))
-    
-
     def binary_search(arr, x, right, left) -> int:
         if right <= left: # промежуток пуст
             return -1
@@ -49,9 +15,32 @@ def broken_search(nums, target) -> int:
         else: # иначе следует искать в правой половине
             return binary_search(arr, x, mid + 1, right)
 
+    def bin_search(arr, x, left, right):
+        arr_len = len(arr)
+        if arr_len < 4:
+            for i in range(arr_len):
+                if arr[i][1] == x:
+                    return arr[i][0]
+            return -1
+
+        # center
+        mid = (left + right) // 2
+
+        if arr[mid][1] > arr[left][1]:
+            sorted = (left, mid)
+            unsorted = (mid + 1, right)
+        else:
+            unsorted = (left, mid - 1)
+            sorted = (mid, right)
+        if arr[sorted[0]] <= x <= arr[sorted[1]]:
+            return binary_search(sorted, x, sorted[0], sorted[1])
+        else:
+            return bin_search(unsorted, x, unsorted[0], unsorted[1])
+        
+    
     inspected = list(enumerate(nums))
     
-    bin_search(inspected, target, 0, len(inspected))
+    return bin_search(inspected, target, 0, len(inspected))
 
 def test():
     arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
