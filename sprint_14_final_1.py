c30aeef92ad8009e8f3e5e8de54df5b586b5b444
Ковылин Василий <coherentus@gmail.com>
2021-09-24 16:43:54 +0300

    jhkjhjk

--- sprint_14_final_1.py
+++ sprint_14_final_1.py
@@ -2,46 +2,91 @@ def broken_search(nums, target) -> int:
     #  Your code
     #  “ヽ(´▽｀)ノ”
     
-    def binary_search(arr, x, right, left) -> int:
+    def binary_search(arr, x, left, right) -> int:
         if right <= left: # промежуток пуст
             return -1
         # промежуток не пуст
         mid = (left + right) // 2
-        if arr[mid][1] == x: # центральный элемент — искомый
-            return arr[mid][0]
-        elif x < arr[mid][1]: # искомый элемент меньше центрального
+        if arr[mid] == x: # центральный элемент — искомый
+            return arr[mid]
+        elif x < arr[mid]: # искомый элемент меньше центрального
                         # значит следует искать в левой половине
             return binary_search(arr, x, left, mid)
         else: # иначе следует искать в правой половине
             return binary_search(arr, x, mid + 1, right)
 
-    def bin_search(arr, x, left, right):
+    def bin_brk_search(arr, x, left, right):
         arr_len = len(arr)
         if arr_len < 4:
             for i in range(arr_len):
-                if arr[i][1] == x:
-                    return arr[i][0]
+                if arr[i] == x:
+                    return arr[i]
             return -1
 
         # center
         mid = (left + right) // 2
 
-        if arr[mid][1] > arr[left][1]:
-            sorted = (left, mid)
-            unsorted = (mid + 1, right)
+        if arr[mid] > arr[left]:
+            sorted_half = (left, mid)
+            unsorted_half = (mid + 1, right)
         else:
-            unsorted = (left, mid - 1)
-            sorted = (mid, right)
-        if arr[sorted[0]] <= x <= arr[sorted[1]]:
-            return binary_search(sorted, x, sorted[0], sorted[1])
+            unsorted_half = (left, mid - 1)
+            sorted_half = (mid, right)
+        if arr[sorted_half[0]] <= x <= arr[sorted_half[1]]:
+            return binary_search(arr, x, sorted_half[0], sorted_half[1])
         else:
-            return bin_search(unsorted, x, unsorted[0], unsorted[1])
+            return bin_brk_search(arr, x, unsorted_half[0], unsorted_half[1])
         
     
-    inspected = list(enumerate(nums))
+    inspected = nums
     
-    return bin_search(inspected, target, 0, len(inspected))
+    return bin_brk_search(inspected, target, 0, len(inspected) - 1)
+
+def main():
+    count_item = int(input())
+    find_x = int(input())
+    items = list(map(int, input().split()))
+    
+    result = broken_search(items, find_x)
+    print(result)
+
 
 def test():
     arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
     assert broken_search(arr, 5) == 6
+
+if __name__ == '__main__':
+    main()
+
+
+
+"""
+9
+5
+19 21 100 101 1 4 5 7 12
+
+2
+1
+5 1
+
+8
+3
+1 2 3 5 6 7 9 0
+
+7
+5
+0 2 6 7 8 9 10
+
+1
+1
+1
+
+3
+8
+3 6 7
+
+11
+1
+1 2 3 4 5 6 7 8 9 10 0
+
+"""
