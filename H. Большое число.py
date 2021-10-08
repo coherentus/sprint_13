dcd490ae1a8cbcd2109336170730924937f836da
Ковылин Василий <coherentus@gmail.com>
2021-10-08 17:01:37 +0300

    new H

--- H. Большое число.py
+++ H. Большое число.py
@@ -1,59 +1,42 @@
-def sort_arr(arr, lf, rg):
-    if rg - lf <= 1:
-        return
-    mid = (lf + rg) // 2
-    sort_arr(arr, lf, mid)
-    sort_arr(arr, mid + 1, rg)
-    arr[lf: rg] = merge(arr, lf, mid, rg)
+def sort_arr(arr):
+    """Cортировать слиянием."""
+    if len(arr) == 1:
+        return arr
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        # разделение на две части
+        lf_segment = sort_arr(arr[left: mid + 1])
+        rg_segment = sort_arr(arr[mid + 1: right + 1])
 
-
-def merge(arr, lf, mid, rg):
-    # left, mid; mid + 1, right
-    first = arr[lf: mid]
-    second = arr[mid: rg]
-    if not first:
-        return second
-    if not second:
-        return first
-    l_first = len(first)
-    l_second = len(second)
-    f_idx = 0
-    s_idx = 0
-    result = [None] * (l_first + l_second)
-    res_idx = 0
-    while True:
-        first_elem = first[f_idx]
-        second_elem = second[s_idx]
-        if int(first_elem + second_elem) > int(second_elem + first_elem):
-            result[res_idx] = first_elem
-            f_idx += 1
+        # объединение частей
+        lf_len = len(lf_segment)
+        rg_len = len(rg_segment)
+        res = [None] * (lf_len + rg_len)
+        lf_idx, rg_idx = 0, 0
+        res_idx = 0
+        while (lf_idx < lf_len) and (rg_idx < rg_len):
+            lf_val = lf_segment[lf_idx]
+            rg_val = rg_segment[rg_idx]
+            if int(lf_val + rg_val) > int(rg_val + lf_val):
+                res[res_idx] = lf_val
+                lf_idx += 1
+            else:
+                res[res_idx] = rg_val
+                rg_idx += 1
             res_idx += 1
-        else:
-            result[res_idx] = second_elem
-            s_idx += 1
-            res_idx += 1
-        if f_idx == l_first:
-            ended = 1
-            break
-        if s_idx == l_second:
-            ended = 2
-            break
-    if ended == 2:
-        result[res_idx:] = first[f_idx:]
-    else:
-        result[res_idx:] = second[s_idx:]
-    return result
+        if lf_idx < lf_len:
+            res[res_idx:] = lf_segment[lf_idx:]
+        if rg_idx < rg_len:
+            res[res_idx:] = rg_segment[rg_idx:]
+        return res
 
 
 def max_glue_num(arr, lenght):
     """Отсортировать, склеить и отдать."""
-    left, right = 0, len(arr) - 1
-    sort_arr(arr, left, right)
-
-    res = ''
-    for item in arr:
-        res = res + item
-    return res
+    left, right = 0, lenght - 1
+    result = sort_arr(arr)
+    return ''.join(result)
 
 
 def main():
